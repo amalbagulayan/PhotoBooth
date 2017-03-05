a4439be137bf0040a3effde68c3406c2d30271fb
amalbagulayan <amal1989@gmail.com>
2017-03-05 15:00:52 -0600

    Initial Commit
    hola!

---
+++ app/src/main/java/com/amalbagulayan/photobooth/PhotoBooth.java
@@ -0,0 +1,108 @@
+package com.amalbagulayan.photobooth;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.net.URI;
+
+public class PhotoBooth extends AppCompatActivity {
+
+    private static String logtag = "PhotoBooth Log" ;
+    private static int TAKE_PICTURE = 1;
+    private Uri imageUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_booth);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton cameraButton = (FloatingActionButton) findViewById(R.id.button_camera);
+        cameraButton.setOnClickListener((View.OnClickListener) cameraListener);
+    }
+
+    private DialogInterface.OnClickListener cameraListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            takePhoto(dialog);
+        }
+    };
+
+    private void takePhoto(DialogInterface dialog)
+    {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File photo =  new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),"picture.jpg");
+        imageUri = Uri.fromFile(photo);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent,TAKE_PICTURE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent intent){
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if(resultCode == Activity.RESULT_OK){
+            Uri selectedImage = imageUri;
+            getContentResolver().notifyChange(selectedImage, null);
+            ImageView imageView = (ImageView)findViewById(R.id.image_camera);
+            ContentResolver cr = getContentResolver();
+            Bitmap bitmap;
+
+            try{
+                bitmap = MediaStore.Images.Media.getBitmap(cr, selectedImage);
+                imageView.setImageBitmap(bitmap);
+                Toast.makeText(PhotoBooth.this,selectedImage.toString(),Toast.LENGTH_LONG).show();
+
+            }catch(Exception e){
+                Log.e(logtag, e.toString());
+            }
+
+        }
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_photo_booth, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
